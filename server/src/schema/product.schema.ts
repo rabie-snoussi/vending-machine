abb0912c7e050d5b61fd704eb67c9f7dfff4b222
Rabie Snoussi <rabie.snoussi.dev@gmail.com>
2021-10-28 06:22:39 +0100

    Changed product min amountAvailable schema

--- server/src/schema/product.schema.ts
+++ server/src/schema/product.schema.ts
@@ -3,7 +3,7 @@ import { object, string, number } from 'yup';
 export const createProductSchema = object({
   body: object({
     productName: string().required('Product name is required'),
-    amountAvailable: number().required('Product amount is required').min(1),
+    amountAvailable: number().required('Product amount is required').min(0),
     cost: number().required('Product cost is required').min(5),
   }),
 });
